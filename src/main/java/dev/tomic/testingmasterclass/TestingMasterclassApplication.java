a944cda47985ea64c450f63a96f871c0cdd19f77
ivantomic77 <ivan.tomic2108@gmail.com>
2024-05-08 21:00:34 +0200

    Initial project and introduction

---
+++ src/main/java/dev/tomic/testingmasterclass/TestingMasterclassApplication.java
@@ -0,0 +1,13 @@
+package dev.tomic.testingmasterclass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingMasterclassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingMasterclassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/tomic/testingmasterclass/TestingMasterclassApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.tomic.testingmasterclass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingMasterclassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
